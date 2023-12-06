ed7e81e3acd17db058b2ce80b7b0692318e8851f
Daniel Jenkins <deiussum@gmail.com>
2023-12-06 08:13:38 -0600

    Solution for Dec 6, 2023 - Part 1

--- 2023/06/main.js
+++ 2023/06/main.js
@@ -3,10 +3,53 @@ const readfile = require('../../common/node/readfile');
 const TEST_INPUT = 'input-test.txt';
 const INPUT = 'input.txt';
 
-readfile.readfile(TEST_INPUT, (lines) => {
+readfile.readfile(INPUT, (lines) => {
     if (lines.length === 0) console.log('No input to process');
 
+    const times = [];
+    const distances = [];
     lines.forEach((line) => {
-        console.log(line);
+        const labelSplit = line.split(':');
+        if (labelSplit.length !== 2) return;
+
+        const label = labelSplit[0];
+        const numbers = parseNumbersToList(labelSplit[1]);
+
+        if (label === "Time") times.push(...numbers);
+        if (label === "Distance") distances.push(...numbers);
     });
+
+    let part1Total = 1;
+    for(let i = 0; i < times.length; i++) {
+        const time = times[i];
+        const distance = distances[i];
+        const waysToWin = getWaysToWin(time, distance);
+
+        part1Total *= waysToWin;
+    }
+
+    console.log(`Part 1 1otal is ${part1Total}`)
 });
+
+const parseNumbersToList = (numberString) => {
+    const numbers = [];
+
+    const numberSplit = numberString.trim().split(' ');
+
+    numberSplit.forEach((number) => {
+        if (number === '') return;
+        numbers.push(Number(number));
+    });
+
+    return numbers;
+}
+
+const getWaysToWin = (time, distance) => {
+    let num1 = 1;
+    while(num1 * (time - num1) <= distance) num1++;
+    const num2 = time - num1;
+    const waysToWin = Math.abs(num2 - num1 + 1);
+
+    console.log(`Time: ${time}, Distance: ${distance}, Num1: ${num1}, Num2: ${num2}, Ways to win: ${waysToWin}`);
+    return waysToWin;
+}
