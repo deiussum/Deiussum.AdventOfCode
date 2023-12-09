949ecc462e50cc80bda1a3d1a7200e0bbbb39c54
Daniel Jenkins <deiussum@gmail.com>
2023-12-08 19:29:50 -0600

    Solution for Dec 8, 2023 - Part 2

--- 2023/08/main.js
+++ 2023/08/main.js
@@ -1,6 +1,7 @@
 const readfile = require('../../common/node/readfile');
 
 const TEST_INPUT = 'input-test.txt';
+const TEST_INPUT2 = 'input-test2.txt';
 const INPUT = 'input.txt';
 
 readfile.readfile(INPUT, (lines) => {
@@ -16,8 +17,15 @@ readfile.readfile(INPUT, (lines) => {
         nodeMap.addNode(line);
     });
 
-    const path = nodeMap.getPath(instructions);
-    console.log(`Path length: ${path.length}`);
+     const path1 = nodeMap.getPath(instructions);
+     console.log(`Path length: ${path1.length}`);
+
+    //const path2Size = nodeMap.getGhostPath(instructions);
+    // There is no logical reason this should have worked, but it does.  It only works because
+    // the puzzle is setup so that the distance to the first end node is the same as the distance
+    // the end node is from itself while following the instructions.
+    const path2Size = nodeMap.getPart2PathLcm(instructions);
+    console.log(`Path length part 2: ${path2Size}`);
 });
 
 class Node {
@@ -26,7 +34,7 @@ class Node {
     #right = '';
 
     constructor(line) {
-        const regex = /^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$/;
+        const regex = /^([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)$/;
         const matches = regex.exec(line);
 
         this.#label = matches[1];
@@ -37,14 +45,22 @@ class Node {
     getLabel() { return this.#label; }
     getLeft() { return this.#left; }
     getRight() { return this.#right; }
+
+    isStartingNode() { return this.#label[2] === 'A'; }
+    isEndingNode() { return this.#label[2] === 'Z'; }
 }
 
 class NodeMap {
     #nodes = {};
+    #startingNodes = [];
+    #endingNodes = [];
 
     addNode(line) {
         const node = new Node(line);
         this.#nodes[node.getLabel()] = node;
+
+        if (node.isStartingNode()) this.#startingNodes.push(node);
+        if (node.isEndingNode()) this.#endingNodes.push(node);
     }
 
     getNode(label) {
@@ -69,6 +85,98 @@ class NodeMap {
         }
 
         return path;
+    }
+
+    getPart2PathLcm(instructions) {
+        const distances = [];
+
+        this.#startingNodes.forEach((node) => {
+            distances.push(this.#getDistanceToFirstEndNode(node, instructions));
+        });
+
+        console.log(distances);
+
+        return this.#getLcm(distances);
+    }
+
+    getGhostPath(instructions) {
+        let pathCount = 0;
+        let currentNodes = this.#startingNodes;
+        let instructionIndex = 0;
+        while(!this.#areAllEndingNodes(currentNodes)) {
+            const nextInstruction = instructions[instructionIndex];
+
+            currentNodes = this.#getNextNodes(nextInstruction, currentNodes);
+            instructionIndex = (instructionIndex + 1) % instructions.length;
+            pathCount++;
+        }
+        return pathCount;
+    }
+
+    #getLcm(numbers) {
+        let maxN = 0;
+
+        numbers.forEach((n) => {
+            if (n > maxN) maxN = n;
+        });
+
+        let multiplier = 1;
+        let testLcm = maxN * multiplier;
+        let keepLooking = true;
+        while(keepLooking) {
+            let found = true;
+            numbers.forEach((n) => {
+                if (testLcm % n !== 0) found = false;
+            });
+
+            keepLooking = !found;
+
+            if (!found) {
+                multiplier++;
+                testLcm = maxN * multiplier;
+            }
+        }
+
+        return testLcm;
+    }
+
+    #getDistanceToFirstEndNode(node, instructions) {
+        let pathCount = 0;
+        let currentNode = node;
+        let instructionIndex = 0;
+        while(!currentNode.isEndingNode()) {
+            const nextInstruction = instructions[instructionIndex];
+            currentNode = this.#getNextNode(nextInstruction, currentNode);
+            instructionIndex = (instructionIndex + 1) % instructions.length;
+            pathCount++;
+        }
+        return pathCount;
+
+    }
+
+    #getNextNode(instruction, node) {
+        if(instruction === 'R') return this.getNode(node.getRight());
+        if(instruction === 'L') return this.getNode(node.getLeft());
+    }
+
+    #getNextNodes(instruction, nodes) {
+        const newNodes = [];
+
+        nodes.forEach((node) => {
+            newNodes.push(this.#getNextNode(node));
+        });
+
+        return newNodes;
+    }
+
+    #areAllEndingNodes(nodes) {
+        let endingCount = 0;
+
+        nodes.forEach((node) => {
+            if (node.isEndingNode()) endingCount++;
+        });
+
+        return endingCount === nodes.length;
     }
 }
 
