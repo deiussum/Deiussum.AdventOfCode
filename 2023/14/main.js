b71897c2acc3d5a95cc81855b6b6e7b2ff2dfb3e
Daniel Jenkins <deiussum@gmail.com>
2023-12-14 14:31:37 -0600

    Solution for Dec 14, 2023 - Part 2

--- 2023/14/main.js
+++ 2023/14/main.js
@@ -14,18 +14,53 @@ readfile.readfile(INPUT, (lines) => {
     platform.tiltNorth();
 
     const part1load = platform.getLoad();
-    console.log(`Part 1 load: ${part1load}`);
+    stopwatch.timelog(`Part 1 load: ${part1load}`);
+
+    const platform2 = new Platform(lines);
+
+    platform2.spinCycle(1000000000);
+
+    const part2load = platform2.getLoad();
+    stopwatch.timelog(`Part 2 load: ${part2load}`);
 
     stopwatch.stop();
 });
 
 class Platform {
     #rows = [];
+    #snapshots = [];
+    #rowCount = 0;
+    #colCount = 0;
 
     constructor(lines) {
         lines.forEach((line) => {
+            // Strings are immutable so store the lines as an array of characters instead
             this.#rows.push(line.split(''));
         });
+        this.#rowCount = this.#rows.length;
+        this.#colCount = this.#rows[0].length;
+    }
+
+    spinCycle(count) {
+        for(let i=0; i<count; i++) {
+            this.tiltNorth();
+            this.tiltWest();
+            this.tiltSouth();
+            this.tiltEast();
+
+            const snapshotIndex = this.#findSnapshot();
+            if (snapshotIndex >= 0) {
+                const cycleLength = i - snapshotIndex;
+                const remainingCycles = Math.floor((count - i)/cycleLength);
+                const remainder = count - i - remainingCycles * cycleLength - 1;
+
+                this.#restoreSnapshot(snapshotIndex + remainder);
+                break;
+            }
+            else {
+                this.#saveSnapshot();
+            }
+        }
     }
 
     tiltNorth() {
@@ -35,6 +70,37 @@ class Platform {
                 const space = row[colIndex];
 
                 if (space === 'O') this.#moveRockN(rowIndex, colIndex);
+            }
+        }
+    }
+
+    tiltSouth() {
+        for(let rowIndex=this.#rowCount - 2; rowIndex >= 0; rowIndex--) {
+            const row = this.#rows[rowIndex];
+            for(let colIndex=0; colIndex<row.length; colIndex++) {
+                const space = row[colIndex];
+
+                if (space === 'O') this.#moveRockS(rowIndex, colIndex);
+            }
+        }
+    }
+
+    tiltWest() {
+        for(let colIndex=1; colIndex<this.#colCount; colIndex++) {
+            for(let rowIndex=0; rowIndex<this.#rowCount; rowIndex++) {
+                const space = this.#rows[rowIndex][colIndex];
+
+                if (space === 'O') this.#moveRockW(rowIndex, colIndex);
+            }
+        }
+    }
+
+    tiltEast() {
+        for(let colIndex=this.#colCount - 2; colIndex >= 0; colIndex--) {
+            for(let rowIndex=0; rowIndex<this.#rowCount; rowIndex++) {
+                const space = this.#rows[rowIndex][colIndex];
+
+                if (space === 'O') this.#moveRockE(rowIndex, colIndex);
             }
         }
     }
@@ -58,4 +124,61 @@ class Platform {
         this.#rows[row][col] = '.';
     }
 
+    #moveRockS(row, col) {
+        let newRow = row + 1;
+        while(newRow < this.#rowCount && this.#rows[newRow][col] === '.') newRow++;
+
+        if (newRow === row + 1) return;
+        this.#rows[newRow - 1][col] = 'O';
+        this.#rows[row][col] = '.';
+    }
+
+    #moveRockW(row, col) {
+        let newCol = col - 1;
+        while(newCol >=0 && this.#rows[row][newCol] === '.') newCol--;
+
+        if (newCol === col - 1) return;
+        this.#rows[row][newCol + 1] = 'O';
+        this.#rows[row][col] = '.';
+    }
+
+    #moveRockE(row, col) {
+        let newCol = col + 1;
+        while(newCol < this.#colCount && this.#rows[row][newCol] === '.') newCol++;
+
+        if (newCol === col + 1) return;
+        this.#rows[row][newCol - 1] = 'O';
+        this.#rows[row][col] = '.';
+    }
+
+    #saveSnapshot() {
+        const snapshot = this.#rows.map((row) => row.join(''));
+        this.#snapshots.push(snapshot);
+        return snapshot;
+    }
+
+    #restoreSnapshot(snapshotIndex) {
+        const snapshot = this.#snapshots[snapshotIndex];
+        this.#rows = snapshot.map((row) => row.split(''));
+    }
+
+    #findSnapshot() {
+        const curSnapshot = this.#rows.map((row) => row.join(''));
+        let foundIndex = -1;
+        this.#snapshots.forEach((snapshot, index) => {
+            if (this.#comparePlatforms(curSnapshot, snapshot)) {
+                foundIndex = index;
+            }
+        });
+        return foundIndex;
+    }
+
+    #comparePlatforms(platform1, platform2) {
+        for(let i=0; i<this.#rowCount; i++) {
+            if (platform1[i] !== platform2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- common/node/stopwatch.js
+++ common/node/stopwatch.js
@@ -4,19 +4,72 @@ class Stopwatch {
 
     start() {
         this.#startTime = new Date();
-        console.log(`Starting at ${this.#startTime}`);
+        this.timelog('Starting...')
     }
 
     stop() {
         const endTime = new Date();
         const elapsed = endTime - this.#startTime;
-
-        console.log(`Ending at ${endTime} - ${elapsed}ms`);
+        this.timelog('Stopped.');
         return elapsed;
     }
+
+    timelog(msg) {
+        const curTime = new Date();
+        const elapsed = curTime - this.#startTime;
+        const dateString = this.formatDate(curTime)
+        const elapsedString = this.elapsedMillisecondsToString(elapsed);
+
+        console.log(`${dateString} (${elapsedString}) - ${msg}`);
+    }
+
+    formatDate(date) {
+        const year = date.getFullYear();
+        const month = this.padLeft('0', date.getMonth() + 1, 2);
+        const day = this.padLeft('0', date.getDate(), 2);
+        const hours = this.padLeft('0', date.getHours(), 2);
+        const mins = this.padLeft('0', date.getMinutes(), 2);
+        const secs = this.padLeft('0', date.getSeconds(), 2);
+        const ms = this.padLeft('0', date.getMilliseconds(), 3);
+        return `${year}-${month}-${day} ${hours}:${mins}:${secs}.${ms}`;
+    }
+
+    padLeft(char, number, padSize) {
+        return (char.repeat(padSize) + number.toString()).slice(-padSize);
+    }
+
+    elapsedMillisecondsToString(ms) {
+        const days = Math.floor(ms / MS_PER_DAY);
+        let remaining = ms - days * MS_PER_DAY;
+
+        const hrs = Math.floor(remaining / MS_PER_HR);
+        remaining -= hrs * MS_PER_HR;
+
+        const mins = Math.floor(remaining / MS_PER_MIN);
+        remaining -= mins * MS_PER_MIN;
+
+        const secs = Math.floor(remaining / MS_PER_SEC);
+        remaining -= secs * MS_PER_SEC;
+
+        const parts = [];
+
+        if (days > 0) parts.push(`${days}d`);
+        if (hrs > 0) parts.push(`${hrs}h`);
+        if (mins > 0) parts.push(`${mins}m`);
+        if (secs > 0) parts.push(`${secs}s`);
+        parts.push(`${remaining}ms`);
+
+        return parts.join(' ');
+    }
 }
+
+const MS_PER_SEC = 1000;
+const MS_PER_MIN = 60 * MS_PER_SEC;
+const MS_PER_HR = 60 * MS_PER_MIN;
+const MS_PER_DAY = 24 * MS_PER_HR;
 
 const stopwatch = new Stopwatch();
 
 exports.start = () => stopwatch.start(); 
 exports.stop = () => stopwatch.stop(); 
+exports.timelog = (msg) => stopwatch.timelog(msg);
